765d8a6b3ecd94420684a869df4388c7eb3762b0
Sibi Prabakaran <sibi@psibi.in>
2017-04-27 05:57:35 +0530

    Add test code

---
+++ test/Spec.hs
@@ -0,0 +1,26 @@
+import Test.Hspec
+import Test.Hspec.Expectations.Contrib (isRight)
+import System.SysInfo
+import Foreign.C.Error
+
+instance Show Errno where
+    show (Errno val) = show val
+
+getRight :: (Show a, Show b) => Either a b -> b
+getRight (Right b) = b
+getRight x@(Left a) = error $ "Invalid value " ++ show x
+
+main :: IO ()
+main =
+  hspec $
+  do describe "sysInfo function" $
+       do it "retuns valid data" $
+            do val <- sysInfo
+               val `shouldSatisfy` isRight
+          it "uptime > 0" $
+            do val <- getRight <$> sysInfo
+               (uptime val) `shouldSatisfy` (> 0)
+          it "freeram > 0" $
+            do val <- getRight <$> sysInfo
+               (freeram val) `shouldSatisfy` (> 0)
+
